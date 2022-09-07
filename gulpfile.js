622e3e1690842c76c1a544d92386264d8f515856
Lakorthus <julio28ve@gmail.com>
2022-09-07 23:08:07 +0100

    Partials and imports

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return src('caimito/**/*.scss')
     .pipe(sass())
-    .pipe(dest('public'))
+    .pipe(dest('public',{ sourcemaps: true }))
 }
 
 function watchTask() {
