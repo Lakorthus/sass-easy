af76539fadfc9f241276ec4c53adc5bd52a0c844
Lakorthus <julio28ve@gmail.com>
2022-09-07 11:41:55 +0100

    project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('*.scss')
+  return src('caimito/**/*.scss')
     .pipe(sass())
-    .pipe(dest('css'))
+    .pipe(dest('public'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['caimito/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
