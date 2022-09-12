e47fc0b483756f0f03f71c4d88e2b8ccaf73a18d
Lakorthus <julio28ve@gmail.com>
2022-09-12 23:45:16 +0100

    Purging css with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('caimito/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('public',{ sourcemaps: true }))
+  .pipe(sass({ outputStyle: 'compressed' }))
+  .pipe(purgecss({ content: ['*.html'] }))
+  .pipe(dest('public',{ sourcemaps: true }))
 }
 
 function watchTask() {
-  watch(['caimito/**/*.scss'], buildStyles)
+  watch(['caimito/**/*.scss','*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
