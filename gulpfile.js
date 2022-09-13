9a160eea58b1ccb695dfc0ca3aaa1e5de51388b3
Lakorthus <julio28ve@gmail.com>
2022-09-13 23:15:04 +0100

    Live Design 1

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ const purgecss = require('gulp-purgecss')
 function buildStyles() {
   return src('caimito/**/*.scss')
   .pipe(sass({ outputStyle: 'compressed' }))
-  .pipe(purgecss({ content: ['*.html'] }))
+  .pipe(purgecss({ content: ['*.html','**/*/*.html'] }))
   .pipe(dest('CSS',{ sourcemaps: true }))
 }
 
 function watchTask() {
-  watch(['caimito/**/*.scss','*.html'], buildStyles)
+  watch(['caimito/**/*.scss','*.html','**/*/*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
